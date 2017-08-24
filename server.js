c8831060524223dd9adebbe8beff95079448b747
Raiiny <benedek200@gmail.com>
2017-08-24 16:07:07 +0200

    Heroku finalize

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hbs = require('hbs');
+const hbs =require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
@@ -11,46 +11,49 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
-
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
   next();
 });
 
 // app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
+//   res.render('maintance.hbs');
 // });
 
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+  return new Date().getFullYear()
 });
 
-hbs.registerHelper('screamIt', (text) => {
+hbs.registerHelper('screamIt', (text) =>{
   return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
+    welcomeMessage: 'Welcome home.',
   });
 });
 
-app.get('/about', (req, res) => {
+app.get('/about',(req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page'
+    pageTitle: 'About Page',
   });
 });
 
-// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    errorMessage: 'Unable to handle request'
+    error: 'Bad page',
+    message: 'Failed to be here.'
   });
 });
 
-app.listen(port, () => {
+app.listen(port, () =>{
   console.log(`Server is up on port ${port}`);
 });
